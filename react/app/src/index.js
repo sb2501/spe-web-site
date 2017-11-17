7137622e5c430abe82bd1dd2ac327b6d51d5b4f9
sboyd <sboyd@csumb.edu>
2017-11-17 17:21:06 +0000

    update

---
+++ react/app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// eslint-disable-next-line
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Root extends React.Component {
+  render() {
+    return (<div>
+        
+    </div>);
+  }
+    
+    
+}
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
+  
